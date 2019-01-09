0e8e77c7a026ebd5f8b0d149ca1ec7b58abeb284
egilson.cabral <egilson.cabral@dataprev.gov.br>
2019-01-09 16:34:51 -0300

    Alterando exibiçao de pagina

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import * as BooksAPI from './BooksAPI';
 
 class BooksApp extends React.Component {
   state = {
@@ -11,52 +12,36 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
+    books:[],
     categories: [
       {
+        id:'currentlyReading',
         categorieName: 'Currently Reading',
-        books:[{
-          bookStyle:{width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'},
-          bookTitle: 'To Kill a Mockingbird',
-          bookAuthor: 'Harper Lee'
-        },
-        {
-          bookStyle:{width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'},
-          bookTitle: `Ender's Game`,
-          bookAuthor: 'Orson Scott Card'
-        }]
+        books:[]
       },
       {
+        id:'wantToRead',
         categorieName: 'Want to Read',
-        books:[{
-          bookStyle:{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' },
-          bookTitle: '1776',
-          bookAuthor: 'David McCullough'
-        },
-        {
-          bookStyle:{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' },
-          bookTitle: `Harry Potter and the Sorcerer's Stone`,
-          bookAuthor: 'J.K. Rowling'
-        }]
+        books:[]
       },
       {
+        id:'read',
         categorieName: 'Read',
-        books:[{
-          bookStyle:{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' },
-          bookTitle: 'The Hobbit',
-          bookAuthor: 'J.R.R. Tolkien'
-        },
-        {
-          bookStyle:{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' },
-          bookTitle: `Oh, the Places You'll Go!`,
-          bookAuthor: 'Seuss'
-        },
-        {
-          bookStyle:{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' },
-          bookTitle: `The Adventures of Tom Sawyer`,
-          bookAuthor: 'Mark Twain'
-        }]
+        books:[]
       }
     ]
+  }
+
+  componentWillMount(){
+    BooksAPI.getAll().then((books) => this.setState(() => {
+      this.state.categories.forEach(categorie => {
+        books.forEach(book => {
+          if (book.shelf.toUpperCase() === categorie.id.toUpperCase()) {
+              categorie.books.push(book);
+          }        
+        });
+      });
+    }))
   }
 
   createBookSection(){
@@ -68,7 +53,7 @@ class BooksApp extends React.Component {
               <li>
                 <div className="book">
                   <div className="book-top">
-                    <div className="book-cover" style={book.bookStyle}></div>
+                    <div className="book-cover" ></div>
                     <div className="book-shelf-changer">
                       <select>
                         <option value="move" disabled>Move to...</option>
@@ -86,7 +71,7 @@ class BooksApp extends React.Component {
         );
       });
         categorieSection.push(
-          <div className="bookshelf">
+          <div className="bookshelf" key={categorie.id}>
             <h2 className="bookshelf-title">{categorie.categorieName}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
